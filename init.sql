ae1c8117a3e08c189347ef38e89efbf8a4d8ef71
2816083598@qq.com <2816083598@qq.com>
2025-05-26 12:41:30 +0800

    更新数据库初始化

--- init.sql
+++ init.sql
@@ -1,2 +1,45 @@
 -- create the database
-create database if not exists bluebell;
+create database if not exists buleball;
+
+use buleball;
+
+-- create the table for user
+CREATE TABLE `user`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `gender` tinyint NULL DEFAULT 0,
+  `create_time` timestamp DEFAULT CURRENT_TIMESTAMP DEFAULT NULL,
+  `update_time` timestamp DEFAULT CURRENT_TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_username`(`username` ASC) USING BTREE,
+  INDEX `idx_user_id`(`user_id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- create the table for community
+CREATE TABLE `community`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `community_id` bigint NOT NULL,
+  `community_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `introduction` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `create_time` timestamp DEFAULT CURRENT_TIMESTAMP DEFAULT NULL,
+  `update_time` timestamp DEFAULT CURRENT_TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_community_name`(`community_name` ASC) USING BTREE,
+  INDEX `community_id`(`community_id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+CREATE TABLE `post`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `post_id` bigint NOT NULL,
+  `title` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `content` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+	`author_id` bigint NOT NULL,
+	`community_id` bigint NOT NULL,
+  `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `status` int NULL DEFAULT 0,
+  `create_time` timestamp DEFAULT CURRENT_TIMESTAMP DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
